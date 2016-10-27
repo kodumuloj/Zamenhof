267199e441c2101b71c94445f4a58d018d908563
uruk <lucius0720@hotmail.com>
2016-10-27 22:37:13 +0800

    script moidfy

--- script/process_file.py
+++ script/process_file.py
@@ -2,27 +2,45 @@
 
 PATH = '../content/Eo - Zamenhof, L.L_ - Fundamenta Krestomatio.md'
 
-def del_empty_line(file_lists):
-    pass
 
-def del_gabage_line(file_lists):
-    pass
 
-def get_file_lists(path):
+def del_garbage_line(lines):
+    def is_garbage(line):
+        if (len(line)==1 and line[0]=='\n'):
+            return True
+        return line.rstrip('\n').isdigit()
+    return list(filter(lambda x:not is_garbage(x),lines))
+
+def get_lines(path):
     with open(path) as f:
         res = f.readlines()
     return res
 
-def deal_diacritic_slipped(file_list):
+def split_para(lines):
+    def is_para_start(line,former_line):
+        start_mark = line.lstrip()[0]
+        end_mark = former_line.lstrip()[-1]
+        return end_mark == '.' and (start_mark.isupper() or start_mark == 'ˆ')
+
+    new_lines=[]
+    res = ''
+    for i in range(len(lines)):
+        res+=lines[i]
+        if i <len(lines)-1 and is_para_start(lines[i+1],lines[i]):
+            new_lines.append(res)
+            res=''
+    return new_lines
+
+def deal_diacritic_slipped(lines):
     pass
 
 
-def write_file(file_list):
+def write_file(lines):
     pass
 
 def main():
-    file_lists = get_file_lists(PATH)
-    res = write_file(del_empty_line(del_gabage_line(deal_diacritic_slipped(file_list))))
+    lines = get_lines(PATH)
+    res = write_file(del_garbage_line(deal_diacritic_slipped(file_list)))
     return res
 
 if __name__ == '__main__':
